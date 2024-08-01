92e80ffe55b48adb39f78096d76ad3c4930002e0
Nguyen Anh Thu <thubackend2022@gmail.com>
2024-08-01 23:58:57 +0700

    fix cart is empty

--- app/src/main/java/com/mssv_71dctm22077/Cart/CartActivity.java
+++ app/src/main/java/com/mssv_71dctm22077/Cart/CartActivity.java
@@ -1,7 +1,9 @@
 package com.mssv_71dctm22077.Cart;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mssv_71dctm22077.R;
 import com.mssv_71dctm22077.adapter.CartAdapter;
 import com.mssv_71dctm22077.model.CartItem;
+import com.mssv_71dctm22077.model.Product;
 import com.mssv_71dctm22077.sqlite.MyDatabaseHelper;
 
 import java.text.DecimalFormat;
@@ -24,6 +27,7 @@ public class CartActivity extends AppCompatActivity {
   private CartAdapter cartAdapter;
   private MyDatabaseHelper databaseHelper;
   private int userId;
+  private Button buttonPlaceOrder;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class CartActivity extends AppCompatActivity {
 
     recyclerViewCart = findViewById(R.id.recyclerViewCart);
     totalPrice = findViewById(R.id.total_price);
+    buttonPlaceOrder = findViewById(R.id.button_place_order);
 
     cartItemList = databaseHelper.getCartItemsByUserId(userId);
 
@@ -43,6 +48,8 @@ public class CartActivity extends AppCompatActivity {
     recyclerViewCart.setLayoutManager(new LinearLayoutManager(this));
 
     updateTotalPrice();
+
+    buttonPlaceOrder.setOnClickListener(v -> placeOrder());
   }
 
   public void updateTotalPrice() {
@@ -51,6 +58,34 @@ public class CartActivity extends AppCompatActivity {
       total += item.getProductPrice() * item.getQuantity();
     }
     totalPrice.setText("Tổng giá: " + total + " VND");
+  }
+
+  private void placeOrder() {
+    if (cartItemList.isEmpty()) {
+      // Giỏ hàng rỗng, hiển thị thông báo
+      Toast.makeText(this, "Giỏ hàng của bạn chưa có sản phẩm nào!", Toast.LENGTH_SHORT).show();
+      return;
+    }
+
+    long orderId = databaseHelper.createOrder(userId);
+
+    for (CartItem cartItem : cartItemList) {
+      int productId = databaseHelper.getProductIdByName(cartItem.getProductName());
+      if (productId != -1) {
+        databaseHelper.createOrderItem(orderId, productId, cartItem.getQuantity());
+      } else {
+        // Xử lý nếu không tìm thấy sản phẩm với tên đó
+        Toast.makeText(this, "Không tìm thấy sản phẩm: " + cartItem.getProductName(), Toast.LENGTH_SHORT).show();
+      }
+    }
+
+    // Sau khi tạo đơn hàng, xóa các mục giỏ hàng hiện tại
+    databaseHelper.clearCart(userId);
+    cartItemList.clear();
+    cartAdapter.notifyDataSetChanged();
+    updateTotalPrice();
+
+    Toast.makeText(this, "Đơn hàng đã được đặt thành công!", Toast.LENGTH_SHORT).show();
   }
 
 }
